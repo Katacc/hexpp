4940816ee4f5e89de4d50acda4ec4fef3a9a36b2
Katacc <katacc@iostria.org>
2025-04-13 04:38:19 +0300

    Added CMake command exports to sync better with LSP's

--- src/commands/sources/cmd_new.cpp
+++ src/commands/sources/cmd_new.cpp
@@ -431,6 +431,7 @@ Error_code cmd_new::write_CMakeLists() {
         cmake_file << "set(CMAKE_CXX_STANDARD ${cxx_version})" << endl;
         cmake_file << "set(CMAKE_CXX_STANDARD_REQUIRED True)" << endl;
         cmake_file << "set(CMAKE_COLOR_DIAGNOSTICS ON)" << endl;
+        cmake_file << "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" << endl;
         cmake_file << "" << endl;
         cmake_file << "# Debug options" << endl;
         cmake_file << "# Uncomment to enable address sanitizing (DOESNT WORK ON WINDOWS)" << endl;
